9ef0c65424fb62a480ddf2ec18fef443856fd316
DiasCarlos2002 <152930537+DiasCarlos2002@users.noreply.github.com>
2024-06-23 18:34:16 +0100

    asd

--- smagia/robots/main_robots.py
+++ smagia/robots/main_robots.py
@@ -1,5 +1,5 @@
 import spade
-from robots_network import RobotAgent
+from robots_mqtt import RobotAgent
 
 
 #robot1@jabbers.one robot1
@@ -12,7 +12,7 @@ async def main():
     robot_network = ['robot1@jabbers.one', 'robot2@jabbers.one', 'robot3@jabbers.one', 'robot4@jabbers.one']
 
     robot1 = RobotAgent("robot1@jabbers.one", "robot1", 500, 150, "robot1", -8.81, 4.10, 10, robot_network
-                        , 'water_station@jabbers.one', 'energy_station@jabbers.one', 2)
+                        , 'water_station@jabbers.one', 'energy_station@jabbers.one', 2, "1")
     await robot1.start(auto_register=True)
 
     robot1.web.start(hostname="127.0.0.1", port="11000")
